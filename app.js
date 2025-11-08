38ca1333b3a36f9c7c15806f6d1558b0162908c1
Lalitdoesfunstuff <S24CSEU0093@bennett.edu.in>
2025-11-08 19:05:56 +0530

    profile-pic utility

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const userModel = require('./models/user');
 const postModel = require('./models/post');
 const jwt = require('jsonwebtoken');
 
+//after multer
+const upload = require("./config/multerConfig");
+
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -35,7 +38,7 @@ app.post('/register', async function(req,res){
             });
             let token = jwt.sign({email : email, userid : user._id}, "secretKey"); //it will store these two things
             res.cookie("token", token); //when you see the register route cookie you'll see
-            res.send(user);
+            res.redirect('/profile');
         } );
     });
 })
@@ -44,6 +47,24 @@ app.post('/register', async function(req,res){
 
 app.get('/login', function(req,res){
     res.render('login');
+})
+//after multer there is also an option to update profilepic so make route 
+
+app.get('/profile/upload', function(req,res){
+    res.render("pfp.ejs");
+})
+
+// debug step
+// app.post('/upload', upload.single("image"),function(req,res){
+//     console.log(req.file);
+// })now we know name comes in req.file.filename
+
+app.post("/upload",upload.single("image"),isLoggedIn, async function(req,res){
+    //find user using loggedin middle ware then update the profilepic name
+    let user = await userModel.findOne({email : req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect('/profile');
 })
 
 app.post('/login', async function(req,res){
@@ -56,7 +77,7 @@ app.post('/login', async function(req,res){
     bcrypt.compare(req.body.password, user.password, function(err,result){
         if(result){
             //make token 
-            let token = jwt.sign({email : req.body.email}, "secretKey");
+            let token = jwt.sign({email : user.email, userid : user._id}, "secretKey");
             res.cookie("token", token);
             res.status(200).redirect("profile");
         }else{
@@ -74,7 +95,7 @@ app.get('/logout', function(req,res){
 //protected route
 app.get('/profile',isLoggedIn, async function(req,res){
     let user = await userModel.findOne({email : req.user.email}).populate("posts");
-     //populate posts field so you get content inside the posts array instead of jus tIDS
+    //populate posts field so you get content inside the posts array instead of jus tIDS
     res.render("profile", {user}); //after populate the user also contains the content
     // res.render('profile');
 })
@@ -88,12 +109,38 @@ app.post('/post', isLoggedIn, async function(req,res){
         user : user._id,
         content : req.body.content
     })
-
+    
     user.posts.push(post);
     await user.save(); 
     res.redirect('/profile');
 })
 
+app.get('/like/:id',isLoggedIn, async function(req,res){
+    let post = await postModel.findOne({_id : req.params.id}).populate("user");
+    console.log("Post likes:", post.likes);
+    console.log("User ID:", req.user.userid);
+    if(post.likes.indexOf(req.user.userid) === -1) { // post ke like wali array me agar ye id nahi hai
+        post.likes.push(req.user.userid);
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1); // if like exits splice/remove 1 like of that user
+    }
+    await post.save();
+    res.redirect('/profile');
+})
+
+app.get('/edit/:id',isLoggedIn, async function(req,res){
+    let user = await postModel.findOne({_id : req.params.id}).populate("user");
+    res.render("edit", {user});
+})
+
+app.post('/update/:id',isLoggedIn, async function(req,res){
+    let user = await postModel.findOneAndUpdate({_id : req.params.id}, {content : req.body.content});
+    res.redirect("/profile");
+})
+app.get('/delete/:id',isLoggedIn, async function(req,res){
+    let post = await postModel.findOneAndDelete({_id : req.params.id});
+    res.redirect('/profile');
+})
 
 //now you need middleware for protected routes 
 function isLoggedIn(req,res,next){
@@ -105,6 +152,7 @@ function isLoggedIn(req,res,next){
         next();
     }
 }
+
 
 
 app.listen(3000);
